d3dd8dffa92ca5dbb2c32271f1471c0bfb55e532
Cristian Comellas Fluxá <ccf200@hotmail.com>
2023-12-12 19:48:17 +0100

    finished project

--- lib/morphological.py
+++ lib/morphological.py
@@ -1,13 +1,6 @@
 import cv2
 
 
-def dilate(image, kernel):
-    return cv2.dilate(kernel)
-
-
-def erode(image, kernel):
-    return cv2.erode(kernel)
-
 def opening(image, kernel):
     return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
 
